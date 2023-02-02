086b20eba8058992558022905b12080f9ad73eaa
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-02 01:23:56 -0700

    solved lab with algorithm notes

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordArray = Array.from(word);
+
+  console.log(wordArray);
+
+  let i = 1;
+  let testArray = [];
+  let testWord = "";
+
+  while(i <= wordArray.length){
+    
+    testArray.push(wordArray[wordArray.length - i]);
+    testWord = testWord + `${wordArray[wordArray.length - i]}`
+    i++;
+  }
+
+  console.log(testArray);
+  console.log(testWord);
+  
+  if(word === testWord){return true;
+  } else {return false;}
+
 }
 
 /* 
-  Add your pseudocode here
+  get word
+  create array from letters in word -> wordArray
+  create new Array using letters from wordArray loaded backwards -> testArray
+  if wordArray = testArray then word isPalindrome
+
+  ended up needed to restore testArray to a string for comparison at end of function.
 */
 
 /*
-  Add written explanation of your solution here
+  loading the word into an array and using a function to reverse it then comparing the two arrays should confirm whether or not it is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +45,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("xrtsstrx")); //does it work with even length strings
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("tacocat"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("beachball"));
 }
 
 module.exports = isPalindrome;
